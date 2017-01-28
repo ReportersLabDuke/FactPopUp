8635c587b5d6883f12479ff7b3ab1e3899db2c33
Gautam Hathi <gkh8@duke.edu>
2017-01-28 00:16:49 -0500

    adding functionality to update livestream url and fixing manifest

--- politifact-extension-client/js/streamspage.js
+++ politifact-extension-client/js/streamspage.js
@@ -1,25 +1,17 @@
 // JavaScript source code
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = process;
 window.onload = function () {
-    if (new Date() > new Date(2016, 9, 10)) {
-        document.getElementById("vid3").removeAttribute("hidden");
-        document.getElementById("vid1").setAttribute("hidden", "true");
-        document.getElementById("vid2").setAttribute("hidden", "true");
-        document.getElementById("container3").removeAttribute("hidden");
-        document.getElementById("container1").setAttribute("hidden", "true");
-        document.getElementById("container2").setAttribute("hidden", "true");
-    } else if (new Date() > new Date(2016, 9, 6)) {
-        document.getElementById("vid2").removeAttribute("hidden");
-        document.getElementById("vid1").setAttribute("hidden", "true");
-        document.getElementById("vid3").setAttribute("hidden", "true");
-        document.getElementById("container2").removeAttribute("hidden");
-        document.getElementById("container1").setAttribute("hidden", "true");
-        document.getElementById("container3").setAttribute("hidden", "true");
-    } else if (new Date() > new Date(2016, 9, 4)) {
-        document.getElementById("vid1").removeAttribute("hidden");
-        document.getElementById("vid2").setAttribute("hidden", "true");
-        document.getElementById("vid3").setAttribute("hidden", "true");
-        document.getElementById("container1").removeAttribute("hidden");
-        document.getElementById("container2").setAttribute("hidden", "true");
-        document.getElementById("container3").setAttribute("hidden", "true");
-    }
+	xhr.open("GET", "https://raw.githubusercontent.com/ReportersLabDuke/FactPopUp/master/livestream_url", true);
+	xhr.send();
+
+}
+
+function process()
+{
+  if (xhr.readyState == 4) {
+	var url = xhr.responseText;
+    console.log(url);
+	document.getElementsByTagName("iframe")[0].setAttribute("src", url);
+  }
 }
